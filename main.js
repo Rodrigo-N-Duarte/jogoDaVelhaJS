f22469a6e6d8ab6e4289081079e0d4b72f231f10
Rodrigo Duarte <103392533+Rodrigo-N-Duarte@users.noreply.github.com>
2023-02-03 17:14:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ window.onload = () => {
   localStorage.setItem('moves', 0)
 }
 
-const jogoDaVelha = (id) => {
-  mark(id)
-}
-
 function mark(id, player) {
   let block = document.querySelector(`#block${id}`)
   if (block.getAttribute('src') == "") {
@@ -20,7 +16,7 @@ function mark(id, player) {
     }
     localStorage.moves++
     if (localStorage.moves >= 5)
-    result()
+      result()
   }
   else {
     alert('Ação impossível de ser executada')
@@ -28,39 +24,33 @@ function mark(id, player) {
 }
 
 const result = () => { // lógica funcional, porém pode ser otimizada em menos linhas de código
-  let block1 = document.querySelector(`#block1`).getAttribute('src')
-  let block2 = document.querySelector(`#block2`).getAttribute('src')
-  let block3 = document.querySelector(`#block3`).getAttribute('src')
-  let block4 = document.querySelector(`#block4`).getAttribute('src')
-  let block5 = document.querySelector(`#block5`).getAttribute('src')
-  let block6 = document.querySelector(`#block6`).getAttribute('src')
-  let block7 = document.querySelector(`#block7`).getAttribute('src')
-  let block8 = document.querySelector(`#block8`).getAttribute('src')
-  let block9 = document.querySelector(`#block9`).getAttribute('src')
+  let block1 = getBlock(1), block2 = getBlock(2), block3 = getBlock(3), block4 = getBlock(4), 
+  block5 = getBlock(5), block6 = getBlock(6), block7 = getBlock(7), block8 = getBlock(8), block9 = getBlock(9)
 
-  if (block1 == block2 && block1 == block3 && block1 != "")
-    gameOver()
-  if (block4 == block5 && block4 == block6 && block4 != "")
-    gameOver()
-  if (block7 == block8 && block7 == block9 && block7 != "")
-    gameOver()
-  if (block1 == block4 && block1 == block7 && block1 != "")
-    gameOver()
-  if (block2 == block5 && block2 == block8 && block2 != "")
-    gameOver()
-  if (block3 == block6 && block3 == block9 && block3 != "")
-    gameOver()
-  if (block1 == block5 && block1 == block9 && block1 != "")
-    gameOver()
-  if (block3 == block5 && block3 == block7 && block3 != "")
-    gameOver()
+    comparision(block1,block2,block3)
+    comparision(block4,block5,block6)
+    comparision(block7,block8,block9)
+    comparision(block1,block4,block7)
+    comparision(block2,block5,block8)
+    comparision(block3,block6,block9)
+    comparision(block1,block5,block9)
+    comparision(block3,block5,block7)
+}
+function getBlock(id){
+  return document.querySelector(`#block${id}`).getAttribute('src')
 }
 
-function gameOver(x) {
+function comparision(a,b,c){
+  if (a == b && a == c && a != "")
+  gameOver()
+}
+
+
+const gameOver = (x) => {
   alert(`Game Over`)
 }
 
-const playAgain = () =>{
+const playAgain = () => {
   location.reload()
 }
 
@@ -68,7 +58,7 @@ const playAgain = () =>{
 document.querySelector('.btnPlayAgain').addEventListener('click', playAgain)
 addEventListener('keypress', (e) => {
   if (e.key == " " ||
-      e.code == "Space") {
+    e.code == "Space") {
     playAgain()
   }
 })
